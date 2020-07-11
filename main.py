08ae9919c014dc3cf1fa3221e83456e0c0bb55e5
Arjun R <50737099+arjunachuravi@users.noreply.github.com>
2020-07-12 01:09:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import os
-import numpy as np
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-import itertools
-import shutil
-import matplotlib.pyplot as plt
-%matplotlib inline
-
 import tensorflow
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
@@ -23,17 +15,29 @@ def top_2_accuracy(y_true, y_pred):
     return top_k_categorical_accuracy(y_true, y_pred, k=2)
 
 
+# SETTING UP THE GENERATORS 
+
+# IMAGE-GENERATORS ACCEPTS THE ORIGINAL DATA,RANDOMLY TRANSFORMING IT AND RETURN ONLY THE NEW TRANSFORMED DATA. [AUGMENTATION]
+
 train_path                  = 'train'
 valid_path                  = 'val'
 test_path                   = 'test'
 
-num_train_samples           = len(os.listdir(train_path+'/'+"measles")) + len(os.listdir(train_path+'/'+"melanoma")) + len(os.listdir(train_path+'/'+"Psoriasis")) + len(os.listdir(train_path+'/'+"ringworm")) 
-num_val_samples             = len(os.listdir(test_path+'/'+"measles")) + len(os.listdir(test_path+'/'+"melanoma")) + len(os.listdir(test_path+'/'+"Psoriasis")) + len(os.listdir(test_path+'/'+"ringworm")) 
+num_train_samples           = ( len(os.listdir(train_path+'/'+"measles")) + 
+                              len(os.listdir(train_path+'/'+"melanoma")) + 
+                              len(os.listdir(train_path+'/'+"Psoriasis")) + 
+                              len(os.listdir(train_path+'/'+"ringworm")) )
+
+num_val_samples             = ( len(os.listdir(test_path+'/'+"measles")) + 
+                              len(os.listdir(test_path+'/'+"melanoma")) + 
+                              len(os.listdir(test_path+'/'+"Psoriasis")) + 
+                              len(os.listdir(test_path+'/'+"ringworm"))  )
 
 
+# DIMENSION OF IMAGE 
 image_size = 224
 
-
+# THE IMAGE GENERATOR [KERAS]
 datagen = ImageDataGenerator(
     rotation_range          = 180,
     width_shift_range       = 0.1,
@@ -60,24 +64,42 @@ test_batches = datagen.flow_from_directory(test_path,
 
 #######################################################################################
 
+# MOBILENET ARCHITECTURE 
+# MOBILENET IS A CNN ARCHITECTURE FOR IMAGE CLASSIFICATION . IT IS FAST AND CUSTOMIZABLE . 
+# USES DEPTH WISE SEPERABLE CONVOLUTION
+
 mobile = tensorflow.keras.applications.mobilenet.MobileNet()
+
+# MODIFY THE MOBILENET
+
+#--EXCLUDE LAST 6 LAYERS
 x = mobile.layers[-6].output
+#--CREATE A DENSE LAYER FOR PREDICTION OF 4 CLASSES 
 x = Dropout(0.25)(x)
 predictions = Dense(4, activation='softmax')(x)
+
 model = Model(inputs=mobile.input, outputs=predictions)
+
+#--LAST 23 LAYERS WILL BE TRAINED AND FREEZING THE WEIGHTS OF REST
 for layer in model.layers[:-23]:
     layer.trainable = False
 
-model.compile(Adam(lr=0.01), loss='categorical_crossentropy', metrics=[categorical_accuracy, top_2_accuracy, top_3_accuracy])
+model.compile(Adam(lr=0.01), loss='categorical_crossentropy', 
+                             metrics=[categorical_accuracy, top_2_accuracy, top_3_accuracy])
+
+# CLASS SENSITIVITY , HERE PSORIASIS CLASS IS THE MOST SENSITIVE
 
 class_weights={
-    0: 3.0,
-    1: 1.5,
-    2: 2.0,
-    3: 2.4,
+    0: 3.0,   # PSORIASIS
+    1: 1.5,   # MEASLES
+    2: 2.0,   # MELANOMA
+    3: 2.4,   # RINGWORM
 }
 
 filepath = "model.h5"
+
+#######################################################################################
+
 checkpoint = ModelCheckpoint(filepath, monitor='val_top_3_accuracy', verbose=1, 
                              save_best_only=True, mode='max')
 
@@ -96,23 +118,17 @@ history = model.fit_generator(train_batches,
 
 #######################################################################################
 
+#---- TESTING THE MODEL ---- #
 
-val_loss, val_cat_acc, val_top_2_acc, val_top_3_acc = model.evaluate_generator(test_batches)
+# model.load_weights('model.h5')
 
-print('val_loss:', val_loss)
-print('val_cat_acc:', val_cat_acc)
-print('val_top_2_acc:', val_top_2_acc)
-print('val_top_3_acc:', val_top_3_acc)
+# val_loss, val_cat_acc, val_top_2_acc, val_top_3_acc = model.evaluate_generator(test_batches)
 
-model.load_weights('model.h5')
+# print('val_loss:', val_loss)
+# print('val_cat_acc:', val_cat_acc)
+# print('val_top_2_acc:', val_top_2_acc)
+# print('val_top_3_acc:', val_top_3_acc)
 
-val_loss, val_cat_acc, val_top_2_acc, val_top_3_acc = model.evaluate_generator(test_batches)
-
-print('val_loss:', val_loss)
-print('val_cat_acc:', val_cat_acc)
-print('val_top_2_acc:', val_top_2_acc)
-print('val_top_3_acc:', val_top_3_acc)
-
-predictions = model.predict_generator(test_batches, verbose=1)
+# predictions = model.predict_generator(test_batches, verbose=1)
 
 #######################################################################################
